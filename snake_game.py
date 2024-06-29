667e39a9f14f4d3e478feedf51e056e6aa84a2ec
John <128350391+alpaOo@users.noreply.github.com>
2024-06-29 09:20:44 +0800

    Rename move_snake function to change_direcction

--- snake_game.py
+++ snake_game.py
@@ -2,7 +2,7 @@ import curses
 import time
 
 
-def move_snake(unicode_key, x, y, max_x, max_y):
+def change_direction(unicode_key, x, y, max_x, max_y):
     if unicode_key == curses.KEY_UP and y > 0:
         y -= 1
     if unicode_key == curses.KEY_RIGHT and x < max_x - 2:
@@ -35,7 +35,7 @@ def main(stdscr):
 
         stdscr.clear()
         stdscr.addstr(y, x, "s")
-        x, y = move_snake(key, x, y, max_x, max_y)
+        x, y = change_direction(key, x, y, max_x, max_y)
         stdscr.refresh()
 
         prev_key = key
